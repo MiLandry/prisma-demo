9f4931f071503005d6c2be57b9fed42b34e9e988
Michael Landry <mlandryviolin@gmail.com>
2019-01-26 01:06:42 -0500

    Add features

--- index.js
+++ index.js
@@ -1,75 +1,67 @@
-// const { prisma } = require("./generated/prisma-client")
-
-// // A `main` function so that we can use async/await
-// async function main() {
-//   // Create a new user with a new post
-//   const newLawyer = await prisma.createLawyer({
-//     person: {
-//       create: {
-//         firstName: "Ben",
-//         lastName: "Finegold",
-//       },
-//     },
-//   })
-//   console.log(`Created new user: ${newLawyer.name} (ID: ${newLawyer.id})`)
-
-//   // Read all users from the database and print them to the console
-//   const allLawyers = await prisma.lawyers()
-//   console.log(allLawyers)
-// }
-
-// main().catch(e => console.error(e))
-
 const { prisma } = require("./generated/prisma-client")
 const { GraphQLServer } = require("graphql-yoga")
 
 const resolvers = {
   Query: {
-    // courtCase(root, args, context) {
-    //   return context.prisma.courtCase({ id: "12345" })
-    // },
-    person(root, args, context) {
-      return context.prisma.person({ id: "person12345" })
+    courtCase(root, args, context) {
+      return context.prisma.courtCase({ id: args.courtCaseId })
+    },
+    courtCases(root, args, context) {
+      return context.prisma.courtCases()
     },
   },
-  // Lawyer: {
-  //   person(lawyer, args, context) {
-  //     return {}
-  //     // return context.prisma
-  //     //   .user({
-  //     //     id: root.id,
-  //     //   })
-  //     //   .posts()
-  //   },
-  // },
-  // Person: {
-  //   firstName(person, args, context) {
-  //     return "foo"
-  //   },
-  //   lastName(person, args, context) {
-  //     return "bar"
-  //   },
-  // },
   Mutation: {
-    createPerson(root, args, context) {
-      return context.prisma.createPerson({
-        firstName: args.firstName,
-        lastName: args.lastName,
+    async createCourtCase(root, args, context) {
+      const courtPlaintiff = await context.prisma.createPerson({
+        firstName: args.plaintiffFirstName,
+        lastName: args.plaintiffLastName,
+      })
+
+      const courtDefendant = await context.prisma.createPerson({
+        firstName: args.defendantFirstName,
+        lastName: args.defendantLastName,
+      })
+
+      const courtLawyer = await context.prisma.createPerson({
+        firstName: args.lawyerFirstName,
+        lastName: args.lawyerLastName,
+      })
+
+      return context.prisma.createCourtCase({
+        plaintiff: {
+          connect: { id: courtPlaintiff.id },
+        },
+        defendant: {
+          connect: { id: courtDefendant.id },
+        },
+        lawyer: {
+          connect: { id: courtLawyer.id },
+        },
       })
     },
-    // createCourtCase(root, args, context) {
-    //   return context.prisma.createCourtCase({
-    //     // plaintiff: {},
-    //     // defendant: {},
-    //     // lawyer: {},
-    //     context.prisma.createPlaintiff({
-    //       context.prisma.createPerson({
-    //         firstName: "foo",
-    //         lastName: "bar",
-    //       })
-    //     })
-    //   })
-    // },
+  },
+  CourtCase: {
+    plaintiff(courtCase, args, context) {
+      return context.prisma
+        .courtCase({
+          id: courtCase.id,
+        })
+        .plaintiff()
+    },
+    defendant(courtCase, args, context) {
+      return context.prisma
+        .courtCase({
+          id: courtCase.id,
+        })
+        .defendant()
+    },
+    lawyer(courtCase, args, context) {
+      return context.prisma
+        .courtCase({
+          id: courtCase.id,
+        })
+        .lawyer()
+    },
   },
 }
 
@@ -81,44 +73,3 @@ const server = new GraphQLServer({
   },
 })
 server.start(() => console.log("Server is running on http://localhost:4000"))
-
-// publishedPosts(root, args, context) {
-//   return context.prisma.posts({ where: { published: true } })
-// },
-// post(root, args, context) {
-//   return context.prisma.post({ id: args.postId })
-// },
-// postsByUser(root, args, context) {
-//   return context.prisma
-//     .user({
-//       id: args.userId,
-//     })
-//     .posts()
-// },
-
-// Post: {
-//   author(root, args, context) {
-//     return context.prisma
-//       .post({
-//         id: root.id,
-//       })
-//       .author()
-//   },
-// },
-
-//mutation stuff
-// title: args.title,
-// author: {
-//   connect: { id: args.userId },
-// },
-//   publish(root, args, context) {
-//     return context.prisma.updatePost({
-//       where: { id: args.postId },
-//       data: { published: true },
-//     })
-//   },
-//   createUser(root, args, context) {
-//     return context.prisma.createUser({ name: args.name })
-//   },
-// },
-// },
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Prisma Demo
+
+## Why
+
+To demo Prisma AND to POC a startup idea; so much for separation of concerns....
+
+## Getting Up and Running
+
+`yarn`
+
+## Run Locally
+
+`docker compose up -d`
+
+`prisma deploy`
+
+`node index.js`
+then browse to
+http://localhost:4000
+
+## Tests
+
+## Resources
+
+front end for POC https://github.com/MiLandry/docket-poc
+
+## License
+
+UNLICENSED
