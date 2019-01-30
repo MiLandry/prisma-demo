0ebc11424fcb1d1cc0ee2befb8932a7423eb9658
Michael Landry <mlandryviolin@gmail.com>
2019-01-30 14:30:48 -0500

    changes to dev

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const { prisma } = require("./generated/prisma-client")
-const { GraphQLServer } = require("graphql-yoga")
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
   Query: {
@@ -8,68 +8,70 @@ const resolvers = {
     },
     courtCases(root, args, context) {
       return context.prisma.courtCases()
-    },
+    }
   },
   Mutation: {
     async createCourtCase(root, args, context) {
       const courtPlaintiff = await context.prisma.createPerson({
         firstName: args.plaintiffFirstName,
-        lastName: args.plaintiffLastName,
+        lastName: args.plaintiffLastName
       })
 
       const courtDefendant = await context.prisma.createPerson({
         firstName: args.defendantFirstName,
-        lastName: args.defendantLastName,
+        lastName: args.defendantLastName
       })
 
       const courtLawyer = await context.prisma.createPerson({
         firstName: args.lawyerFirstName,
-        lastName: args.lawyerLastName,
+        lastName: args.lawyerLastName
       })
 
       return context.prisma.createCourtCase({
         plaintiff: {
-          connect: { id: courtPlaintiff.id },
+          connect: { id: courtPlaintiff.id }
         },
         defendant: {
-          connect: { id: courtDefendant.id },
+          connect: { id: courtDefendant.id }
         },
         lawyer: {
-          connect: { id: courtLawyer.id },
-        },
+          connect: { id: courtLawyer.id }
+        }
       })
-    },
+    }
   },
   CourtCase: {
     plaintiff(courtCase, args, context) {
       return context.prisma
         .courtCase({
-          id: courtCase.id,
+          id: courtCase.id
         })
         .plaintiff()
     },
     defendant(courtCase, args, context) {
       return context.prisma
         .courtCase({
-          id: courtCase.id,
+          id: courtCase.id
         })
         .defendant()
     },
     lawyer(courtCase, args, context) {
       return context.prisma
         .courtCase({
-          id: courtCase.id,
+          id: courtCase.id
         })
         .lawyer()
-    },
-  },
+    }
+  }
 }
 
 const server = new GraphQLServer({
-  typeDefs: "./schema.graphql",
+  typeDefs: './schema.graphql',
   resolvers,
   context: {
-    prisma,
-  },
+    prisma
+  }
 })
-server.start(() => console.log("Server is running on http://localhost:4000"))
+
+// eslint-disable-next-line no-console
+server.start(() => console.log('Server is running on http://localhost:4000'))
